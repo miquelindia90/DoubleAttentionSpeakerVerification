32a60d2d4eae6fc8cc313b14389710c50df87110
Miquel India <mindia@verbio.com>
2020-08-15 15:21:26 +0100

    Added New AMSoftmax Version

--- scripts/loss.py
+++ scripts/loss.py
@@ -1,6 +1,8 @@
 import torch
 from torch import nn
 
+
+
 class AMSoftmax(nn.Module):
     '''
     additive margin softmax as proposed in:
@@ -26,6 +28,38 @@ class AMSoftmax(nn.Module):
         return -torch.mean(L)
 
 
+class AMSoftmaxV2(nn.Module):
+
+    '''
+    Alternative Implementation Extracted From
+    https://github.com/clovaai/voxceleb_trainer/blob/master/loss/cosface.py
+    '''
+
+    def __init__(self, in_feats, n_classes, m=0.3, s=15):
+        super(AMSoftmaxV2, self).__init__()
+        self.m = m
+        self.s = s
+        self.in_feats = in_feats
+        self.W = torch.nn.Parameter(torch.randn(in_feats, n_classes), requires_grad=True)
+        nn.init.xavier_normal_(self.W, gain=1)
+
+
+    def forward(self, x, label=None):
+        assert x.size()[0] == label.size()[0]
+        assert x.size()[1] == self.in_feats
+        x_norm = torch.norm(x, p=2, dim=1, keepdim=True).clamp(min=1e-12)
+        x_norm = torch.div(x, x_norm)
+        w_norm = torch.norm(self.W, p=2, dim=0, keepdim=True).clamp(min=1e-12)
+        w_norm = torch.div(self.W, w_norm)
+        costh = torch.mm(x_norm, w_norm)
+        label_view = label.view(-1, 1)
+        if label_view.is_cuda: label_view = label_view.cpu()
+        delt_costh = torch.zeros(costh.size()).scatter_(1, label_view, self.m)
+        if x.is_cuda: delt_costh = delt_costh.cuda()
+        costh_m = costh - delt_costh
+        costh_m_s = self.s * costh_m
+        return costh_m_s
+ 
 class FocalSoftmax(nn.Module):
     ''' 
     Focal softmax as proposed in:
