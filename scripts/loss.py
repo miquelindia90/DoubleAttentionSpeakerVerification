46a1def90a52da23f33c90fd2230d4f7f7a65d09
Miquel India <mindia@verbio.com>
2020-08-30 12:50:23 +0100

    Added Annhealing in AM

--- scripts/loss.py
+++ scripts/loss.py
@@ -19,10 +19,20 @@ class AMSoftmax(nn.Module):
         self.W = torch.nn.Parameter(torch.randn(in_feats, n_classes), requires_grad=True)
         nn.init.xavier_normal_(self.W, gain=1)
 
-    def increaseMarginFactor(self):
-        self.m = min(self.m + 0.1, 0.5)
-        
-    def forward(self, x, label=None):
+    def getAnnealedFactor(self,step):
+        alpha = self.__getAlpha(step)
+        return 1/(1+alpha)
+
+    def __getAlpha(self,step):
+        return max(0, 1000./(pow(1.+0.0001*float(step),5.)))        
+
+    def __getCombinedCosth(self, costh, costh_m, step):
+
+        alpha = self.__getAlpha(step)
+        costh_combined = costh_m + alpha*costh
+        return costh_combined/(1+alpha)
+
+    def forward(self, x, label=None, step=0):
         assert x.size()[0] == label.size()[0]
         assert x.size()[1] == self.in_feats
         x_norm = torch.norm(x, p=2, dim=1, keepdim=True).clamp(min=1e-12)
@@ -35,8 +45,9 @@ class AMSoftmax(nn.Module):
         delt_costh = torch.zeros(costh.size()).scatter_(1, label_view, self.m)
         if x.is_cuda: delt_costh = delt_costh.cuda()
         costh_m = costh - delt_costh
-        costh_m_s = self.s * costh_m
-        return costh_m_s
+        costh_combined = self.__getCombinedCosth(costh, costh_m, step)
+        costh_m_s = self.s * costh_combined
+        return costh_m_s 
  
 class FocalSoftmax(nn.Module):
     ''' 
